d44c50404dff14400619253390fea5bea7d07c3c
Jakub Zyznowski <j.zyznowski123@gmail.com>
2024-02-18 11:51:25 +0100

    t

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,7 @@
 const power  = require('./app')
+const os = require('os')
 
 let a = power(3, 3)
 
 console.log(a)
+console.log(os.freemem())
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,36 @@
 const http = require('http')
+const fs = require('fs')
+
 
 const server = http.createServer((req, res) => {
-    
+    console.log("tak")
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>czarnuch</h1> <hr>')
+    if(req.url == '/'){
+    fs.readFile('./index.html',  function(err ,data){
+        if (err){
+            console.log(err)
+            res.end()
+        } else {
+            res.end(data)
+        }
+    })
+} else if (req.url == '/about'){
+    fs.readFile('./about.html' , (err, data) =>{
+        err ? console.log(err) : res.end(data) 
+    })
+} else {
+    fs.readFile('./404.html' , (err, data) => {
+        err ? console.log(err) : res.end(data)
+    })
+}
+
 
 
 })
 
-server.listen(8000)
+server.listen(9000 , 'localhost' , function(){
+    console.log("nie")
+});
+
+
